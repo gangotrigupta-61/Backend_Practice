3854c1098faae8249560e5e5f92dc43ea82d3ccc
gangotrigupta-61 <gangotrigupta61@gmail.com>
2026-09-04 20:06:16 +0530

    npmfiglet_package_require_import

--- math.js
+++ math.js
@@ -1,21 +1,17 @@
-const sum=(a,b)=> a+b;
+module.exports.sum=(a,b)=> a+b;
 const mul=(a,b)=> a*b;
 const div=(a,b)=> a/b;
 
-module.exports.g=9.8;
+g=9.8;
 
 const PI=3.14;
 
 
-let obj={
-    sum:sum,
-    mul:mul,
-    div:div,
+// let obj={
+//     sum:sum,
+//     mul:mul,
+//     div:div,
    
-    PI:PI,
-};
+//     PI:PI,
+// };
 
-
-// module.exports="hello!!";
-
-// module.exports=obj;
