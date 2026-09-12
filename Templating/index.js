460dcd19c37881c4eabd4a24d45a26acdda017e4
gangotrigupta-61 <gangotrigupta61@gmail.com>
2026-09-12 20:22:01 +0530

    InterpolationSyntaxINejs

--- Templating/index.js
+++ Templating/index.js
@@ -1,10 +1,22 @@
 const express=require("express");
 const app=express();
-
 let port=3000;
-app.listen(()=>{
-    console.log("Listening on Port ");
-    console.log("Lets start with templating!!");
+
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
 });
+
+app.get("/hello",(req,res)=>{
+    res.send("hello");
+});
+
+
+
+app.listen(port,()=>{
+    console.log(`Listening on Port ${port}`);
+});
+
+
 
 
