b742c5236dccd4bbbdb36eedae7391a102eed657
gangotrigupta-61 <gangotrigupta61@gmail.com>
2026-09-21 23:39:07 +0530

    Basic

--- REST/index.js
+++ REST/index.js
@@ -7,6 +7,12 @@ app.get("/",(req,res)=>{
     res.send("Server working well!!");
 });
 
+const path= require("path");
+app.use(express.urlencoded({extended:true}));
+app.set("viewengine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
 
 app.listen(PORT,()=>{
     console.log(`Listening on Port ${PORT}`);
