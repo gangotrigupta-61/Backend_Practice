e3e09ab12782212ec313aa101f268780a164fdb9
gangotrigupta-61 <gangotrigupta61@gmail.com>
2026-09-23 20:09:13 +0530

    getsinglepost

--- REST/index.js
+++ REST/index.js
@@ -51,6 +51,14 @@ app.post("/posts",(req,res)=>{
   res.redirect("/posts");
 });
 
+app.get("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let post= posts.find((p)=> id==p.id);
+    res.render("show.ejs",{post});
+});
+
 app.listen(PORT,()=>{
     console.log(`Listening on Port ${PORT}`);
 });
+
+
